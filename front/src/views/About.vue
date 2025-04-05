17b2b21a0568ec63896ce8e8e9b7e3575be53be4
achillllllllle <oscar77.fonty@gmail.com>
2025-04-06 00:15:57 +0200

    setup front back and docker compose

---
+++ front/src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About FinanceGuy</h1>
+    <p>Your personal finance management application</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutView'
+}
+</script> 
